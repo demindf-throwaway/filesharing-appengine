6f3a145cd253493cf64c3784fb35cbca5264fda6
Demin Dmitriy <demindf@gmail.com>
2015-02-01 21:13:43 +0300

    Added chained constructors for DatabaseException

--- fileshare/src/main/java/ru/itmo/fileshare/DatabaseException.java
+++ fileshare/src/main/java/ru/itmo/fileshare/DatabaseException.java
@@ -3,12 +3,8 @@ package ru.itmo.fileshare;
 public class DatabaseException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	public DatabaseException() {
-		super();
-	}
-	
-	public DatabaseException(String message) {
-		super(message);
-	}
-
+	public DatabaseException() { super(); }
+	public DatabaseException(String message) { super(message); }
+	public DatabaseException(String message, Throwable cause) { super(message, cause); }
+	public DatabaseException(Throwable cause) { super(cause); }
 }
